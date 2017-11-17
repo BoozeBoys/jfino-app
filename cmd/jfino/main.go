2d291ed6d20c9f40711b79c3a1e4e014c74b8375
Lorenzo Masini <rugginoso@develer.com>
2017-11-17 15:01:55 +0100

    main: updated example.

--- cmd/jfino/main.go
+++ cmd/jfino/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/BoozeBoys/jfino-app/commander"
 	"github.com/tarm/serial"
@@ -29,17 +30,34 @@ func main() {
 	}
 	defer s.Close()
 
+	time.Sleep(2 * time.Second)
+
 	c := commander.New(s)
 
 	if err := c.Power(true); err != nil {
 		log.Fatal("setting power: ", err)
 	}
 
-	if err := c.Speed(255, 255); err != nil {
+	if err := c.Speed(20, 20); err != nil {
 		log.Fatal("setting speed: ", err)
 	}
 
 	status, err := c.Status()
+	if err != nil {
+		log.Fatal("read status: ", err)
+	}
+
+	for _, line := range status {
+		fmt.Println(string(line))
+	}
+
+	time.Sleep(5 * time.Second)
+
+	if err := c.Speed(0, 0); err != nil {
+		log.Fatal("setting speed: ", err)
+	}
+
+	status, err = c.Status()
 	if err != nil {
 		log.Fatal("read status: ", err)
 	}
