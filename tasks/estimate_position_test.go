4f0be59e6da7dfc5a311095ade1846f3148aab1e
Francesco Sacchi <batt@develer.com>
2017-11-26 12:06:03 +0100

    EstimatePosition: refactor point to simplify; add first ComputePosition implementation.

--- tasks/estimate_position_test.go
+++ tasks/estimate_position_test.go
@@ -20,21 +20,21 @@ func TestErrorRms(t *testing.T) {
 	d := 1.0
 	anchors := make(map[uint]loc.Point)
 	assign := func() {
-		anchors[0] = loc.Point{X: -d, Y: 0, Z: 0}
-		anchors[1] = loc.Point{X: 0, Y: d, Z: 0}
-		anchors[2] = loc.Point{X: d, Y: 0, Z: 0}
-		anchors[3] = loc.Point{X: 0, Y: -d, Z: 0}
+		anchors[0] = loc.Point{-d, 0, 0}
+		anchors[1] = loc.Point{0, d, 0}
+		anchors[2] = loc.Point{d, 0, 0}
+		anchors[3] = loc.Point{0, -d, 0}
 	}
 
 	assign()
 	ep := tasks.NewEstimatePosition(anchors)
 
-	rms := ep.ErrorRms(ranges, loc.Point{X: 0, Y: 0})
+	rms := ep.ErrorRms(ranges, loc.Point{0, 0})
 	if math.Abs(rms-0) > 1e-6 {
 		t.Fatalf("rms: %f", rms)
 	}
 
-	rms = ep.ErrorRms(ranges, loc.Point{X: 1, Y: 1})
+	rms = ep.ErrorRms(ranges, loc.Point{1, 1})
 	res := math.Sqrt((math.Pow(math.Sqrt(1+4)-1, 2) * 2) / 4)
 	if math.Abs(rms-res) > 1e-6 {
 		t.Fatalf("rms: %f", rms)
@@ -42,7 +42,7 @@ func TestErrorRms(t *testing.T) {
 
 	d = 3.0
 	assign()
-	rms = ep.ErrorRms(ranges, loc.Point{X: 0, Y: 0})
+	rms = ep.ErrorRms(ranges, loc.Point{0, 0})
 	if math.Abs(rms-2) > 1e-6 {
 		t.Fatalf("rms: %f", rms)
 	}
@@ -58,10 +58,10 @@ func TestFindBoundingBox(t *testing.T) {
 	d := 1.0
 	anchors := make(map[uint]loc.Point)
 	assign := func() {
-		anchors[0] = loc.Point{X: -d, Y: 0, Z: 1}
-		anchors[1] = loc.Point{X: 0, Y: d, Z: 1}
-		anchors[2] = loc.Point{X: d, Y: 0, Z: 1}
-		anchors[3] = loc.Point{X: 0, Y: -d, Z: 1}
+		anchors[0] = loc.Point{-d, 0, 1}
+		anchors[1] = loc.Point{0, d, 1}
+		anchors[2] = loc.Point{d, 0, 1}
+		anchors[3] = loc.Point{0, -d, 1}
 	}
 
 	assign()
@@ -69,11 +69,11 @@ func TestFindBoundingBox(t *testing.T) {
 
 	b := ep.FindBoundingBox(ranges)
 
-	if !b.P0.IsEqual(loc.Point{X: -11, Y: -11, Z: -9}) {
-		t.Fatalf("box: %v", b)
+	if !b.P0.IsEqual(loc.Point{-11, -11, -9}) {
+		t.Fatalf("bo %v", b)
 	}
 
-	if !b.P1.IsEqual(loc.Point{X: 11, Y: 11, Z: 11}) {
-		t.Fatalf("box: %v", b)
+	if !b.P1.IsEqual(loc.Point{11, 11, 11}) {
+		t.Fatalf("bo %v", b)
 	}
 }
