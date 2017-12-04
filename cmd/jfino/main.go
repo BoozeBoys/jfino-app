22ec2a0ea05d3e65333b6aca79210a7ec970dd91
Francesco Sacchi <batt@develer.com>
2017-12-04 20:25:33 +0100

    main: load configuration from file.

--- cmd/jfino/main.go
+++ cmd/jfino/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
+
+	"github.com/BoozeBoys/jfino-app/loc"
 
 	"github.com/BoozeBoys/jfino-app/commander"
 	"github.com/BoozeBoys/jfino-app/state"
@@ -13,9 +17,33 @@ import (
 	"github.com/tarm/serial"
 )
 
+type Config struct {
+	SerialDevice string
+	BaudRate     int
+	Anchors      map[int]loc.Point
+}
+
+func (c *Config) String() string {
+	return fmt.Sprintf("SerialDevice: %s\nBaudRate: %d\nAnchors: %v\n", c.SerialDevice, c.BaudRate, c.Anchors)
+}
+
+func loadConfig(path string) (*Config, error) {
+	configData, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := json.Unmarshal(configData, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "usage: %s [FILE]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "usage: %s [CONFIG FILE]\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 
@@ -26,7 +54,14 @@ func main() {
 		return
 	}
 
-	s, err := serial.OpenPort(&serial.Config{Name: flag.Arg(0), Baud: 115200})
+	config, err := loadConfig(flag.Arg(0))
+	if err != nil {
+		log.Fatalf("error loading config: %s", err)
+	}
+
+	fmt.Println(config)
+
+	s, err := serial.OpenPort(&serial.Config{Name: config.SerialDevice, Baud: config.BaudRate})
 	if err != nil {
 		log.Fatal("open port: ", err)
 	}
@@ -38,6 +73,7 @@ func main() {
 
 	taskList := []tasks.Task{
 		tasks.NewUpdateStatus(c),
+		tasks.NewEstimatePosition(config.Anchors),
 		tasks.NewSendCommands(c),
 	}
 
