1a32fbd5930eaec09d86addf3ac7f53fecfd62a2
Francesco Sacchi <batt@develer.com>
2018-01-08 22:16:05 +0100

    Feature/slack task (#16)
    tasks: slackbot.

--- slackbot/bot.go
+++ slackbot/bot.go
@@ -4,13 +4,19 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/nlopes/slack"
 )
 
+var ErrEmptyQueue = errors.New("no commands left")
+
 type Slackbot struct {
 	api *slack.Client
 	rtm *slack.RTM
+
+	m        sync.Mutex
+	messages []BotMsg
 }
 
 type BotMsg struct {
@@ -21,46 +27,52 @@ type BotMsg struct {
 }
 
 func New(token string) *Slackbot {
-	sb := &Slackbot{}
+	sb := new(Slackbot)
 	sb.api = slack.New(token)
 	sb.rtm = sb.api.NewRTM()
 	go sb.rtm.ManageConnection()
 	return sb
 }
 
-func (sb *Slackbot) Recv() (*BotMsg, error) {
-
+func (sb *Slackbot) Start() {
 	for {
-		select {
-		case msg := <-sb.rtm.IncomingEvents:
-			switch ev := msg.Data.(type) {
-			case *slack.MessageEvent:
-				info := sb.rtm.GetInfo()
-				botname := fmt.Sprintf("<@%s> ", info.User.ID)
+		msg := <-sb.rtm.IncomingEvents
 
-				_, err1 := sb.api.GetChannelInfo(ev.Channel)
-				_, err2 := sb.api.GetGroupInfo(ev.Channel)
-				isDirectMsg := err1 != nil && err2 != nil
+		switch ev := msg.Data.(type) {
+		case *slack.MessageEvent:
+			info := sb.rtm.GetInfo()
+			botname := fmt.Sprintf("<@%s> ", info.User.ID)
 
-				if ev.User != info.User.ID &&
-					(strings.HasPrefix(ev.Text, botname) || isDirectMsg) {
-					return &BotMsg{ev.Channel, ev.User, strings.TrimPrefix(ev.Text, botname), isDirectMsg}, nil
-				}
+			_, err1 := sb.api.GetChannelInfo(ev.Channel)
+			_, err2 := sb.api.GetGroupInfo(ev.Channel)
+			isDirectMsg := err1 != nil && err2 != nil
 
-			case *slack.RTMError:
-				return nil, errors.New("RTM error")
-
-			case *slack.InvalidAuthEvent:
-				return nil, errors.New("Invalid authentication")
-
-			default:
-				//Take no action
+			if ev.User != info.User.ID &&
+				(strings.HasPrefix(ev.Text, botname) || isDirectMsg) {
+				sb.m.Lock()
+				sb.messages = append(sb.messages, BotMsg{ev.Channel, ev.User, strings.TrimPrefix(ev.Text, botname), isDirectMsg})
+				sb.m.Unlock()
 			}
 		}
 	}
 }
 
-func (sb *Slackbot) Reply(recv *BotMsg, msg string) {
+func (sb *Slackbot) GetLastMessage() (BotMsg, error) {
+	sb.m.Lock()
+	defer sb.m.Unlock()
+
+	var msg BotMsg
+
+	if len(sb.messages) == 0 {
+		return msg, ErrEmptyQueue
+	}
+
+	msg, sb.messages = sb.messages[0], sb.messages[1:]
+
+	return msg, nil
+}
+
+func (sb *Slackbot) Reply(recv BotMsg, msg string) {
 	var s string
 	if recv.DirectMsg {
 		s = msg
---
+++ tasks/read_user_command.go
@@ -0,0 +1,108 @@
+package tasks
+
+import (
+	"encoding/json"
+	"strconv"
+	"strings"
+
+	"github.com/BoozeBoys/jfino-app/slackbot"
+	"github.com/BoozeBoys/jfino-app/state"
+)
+
+type ReadUserCommand struct {
+	bot *slackbot.Slackbot
+}
+
+func NewReadUserCommand(slackToken string) *ReadUserCommand {
+	bot := slackbot.New(slackToken)
+	go bot.Start()
+
+	return &ReadUserCommand{bot: bot}
+}
+
+func (t *ReadUserCommand) Perform(s *state.State) error {
+	msg, err := t.bot.GetLastMessage()
+	if err != nil {
+		return nil
+	}
+
+	tokens := strings.Split(msg.Msg, " ")
+	if len(tokens) == 0 {
+		t.bot.Reply(msg, "?")
+		return nil
+	}
+
+	switch strings.ToLower(tokens[0]) {
+	case "accenditi":
+		s.Power = true
+		t.bot.Reply(msg, "OK :smile:")
+	case "spengiti":
+		s.Power = false
+		t.bot.Reply(msg, "OK :smile:")
+	case "fermati":
+		fallthrough
+	case "stop":
+		s.Motors[0].Speed = 0
+		s.Motors[1].Speed = 0
+		t.bot.Reply(msg, "OK :smile:")
+	case "vai":
+		if len(tokens[1:]) != 1 {
+			t.bot.Reply(msg, "forse volevi dire _vai piano_ o _vai sodo_")
+			return nil
+		}
+		switch tokens[1] {
+		case "piano":
+			s.Motors[0].Speed = 50
+			s.Motors[1].Speed = 50
+			t.bot.Reply(msg, "OK :smile:")
+		case "sodo":
+			s.Motors[0].Speed = 255
+			s.Motors[1].Speed = 255
+			t.bot.Reply(msg, "OK :smile:")
+		default:
+			t.bot.Reply(msg, "forse volevi dire _vai piano_ o _vai sodo_")
+		}
+
+	case "power":
+		if len(tokens[1:]) != 1 {
+			t.bot.Reply(msg, "forse volevi dire _power 1_ o _power 0_")
+			return nil
+		}
+		s.Power = tokens[1] == "1"
+		t.bot.Reply(msg, "OK :smile:")
+
+	case "speed":
+		if len(tokens[1:]) != 2 {
+			t.bot.Reply(msg, "forse volevi dire _speed 255 255_")
+			return nil
+		}
+
+		speed1, err := strconv.Atoi(tokens[1])
+		if err != nil {
+			t.bot.Reply(msg, "il valore del motore 1 deve essere tra -255 e 255")
+			return nil
+		}
+
+		speed2, err := strconv.Atoi(tokens[2])
+		if err != nil {
+			t.bot.Reply(msg, "il valore del motore 2 deve essere tra -255 e 255")
+			return nil
+		}
+
+		s.Motors[0].Speed = speed1
+		s.Motors[1].Speed = speed2
+
+		t.bot.Reply(msg, "OK :smile:")
+	case "status":
+		data, err := json.Marshal(s)
+		if err != nil {
+			t.bot.Reply(msg, "non ci riesco :scream:")
+
+		}
+		t.bot.Reply(msg, string(data))
+	default:
+		t.bot.Reply(msg, "non ho capito! :scream:")
+	}
+
+	return nil
+}
